cf4b15eb92455707d983e91ea4f40f3f8e9b3cb6
Favour41 <91343288+Favour41@users.noreply.github.com>
2026-06-14 02:17:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("This is a demo website. Checkout functionality will be added later.");
+    });
+});
